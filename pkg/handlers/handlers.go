3859f93971202bd81acfd9961ac54c13da1acd1b
Edigiraldo <edison.giraldoa12@gmail.com>
2023-02-05 09:48:51 -0500

    include comments to handler

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -26,6 +26,12 @@ type ErrorBody struct {
 	ErrorMsg *string `json:"error_msg,omitempty"`
 }
 
+// @Summary Handles creation of users
+// @Tags go-serverless-api
+// @Success 201 {object} users.User{} "user created"
+// @Failure 400 {object} ErrorBody
+// @Failure 500 {object} ErrorBody
+// @Param category body users.User{} true "info of user to be created"
 func CreateUser(req events.APIGatewayProxyRequest, tablename string, dynamoCli dynamodbiface.DynamoDBAPI) (res *events.APIGatewayProxyResponse, err error) {
 	body := []byte(req.Body)
 
@@ -61,6 +67,13 @@ func CreateUser(req events.APIGatewayProxyRequest, tablename string, dynamoCli d
 	return ApiResponse(http.StatusCreated, jsonUser)
 }
 
+// @Summary Gets an user by email
+// @Tags go-serverless-api
+// @Success 200 {object} users.User{} "user found"
+// @Failure 400 {object} ErrorBody "the email format is invalid"
+// @Failure 404 {object} ErrorBody "the user was not found"
+// @Failure 500 {object} ErrorBody
+// @Param email query string true "indicates the email of user to delete"
 func GetUser(req events.APIGatewayProxyRequest, tablename string, dynamoCli dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
 	email := req.QueryStringParameters["email"]
 	if !validators.IsEmailValid(email) {
@@ -101,6 +114,12 @@ func GetUser(req events.APIGatewayProxyRequest, tablename string, dynamoCli dyna
 	return ApiResponse(http.StatusOK, jsonUser)
 }
 
+// @Summary Handles user update
+// @Tags go-serverless-api
+// @Success 200 {object} users.User{} "user updated"
+// @Failure 400 {object} ErrorBody "sent user struct is not valid"
+// @Failure 500 {object} ErrorBody
+// @Param category body users.User{} true "info to be updated for a user"
 func UpdateUser(req events.APIGatewayProxyRequest, tablename string, dynamoCli dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
 	body := []byte(req.Body)
 
@@ -134,9 +153,15 @@ func UpdateUser(req events.APIGatewayProxyRequest, tablename string, dynamoCli d
 		return ApiResponse(http.StatusInternalServerError, errBody)
 	}
 
-	return ApiResponse(http.StatusCreated, jsonUser)
+	return ApiResponse(http.StatusOK, jsonUser)
 }
 
+// @Summary Handles user deletion
+// @Tags go-serverless-api
+// @Success 204 {object} users.User{} "user deleted"
+// @Failure 400 {object} ErrorBody "the email format is invalid"
+// @Failure 500 {object} ErrorBody
+// @Param category body users.User{} true "info to be updated for a user"
 func DeleteUser(req events.APIGatewayProxyRequest, tablename string, dynamoCli dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
 	email := req.QueryStringParameters["email"]
 	if !validators.IsEmailValid(email) {
@@ -166,13 +191,9 @@ func DeleteUser(req events.APIGatewayProxyRequest, tablename string, dynamoCli d
 	return ApiResponse(http.StatusNoContent, nil)
 }
 
-/**
- * Handles requests with unexpected methods
- *
- * @param req events.APIGatewayProxyRequest - request received
- *
- * @returns *events.APIGatewayProxyResponse - response
- */
+// @Summary Handles requests with unexpected methods
+// @Tags go-serverless-api
+// @Failure 405 {object} ErrorBody "the method is not allowed"
 func UnhandledMethod(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	errBody := ErrorBody{
 		ErrorMsg: &ErrMethodNotAllowed,
